af2f1cc83e513b2c50ad7f08c1feb7f95ba69e50
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-10-22 21:12:05 +0200

    cars CRUD

--- src/main/java/controller/CarsController.java
+++ src/main/java/controller/CarsController.java
@@ -9,31 +9,61 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.soap.Addressing;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-//@WebServlet(name = "CarsController")
 @WebServlet("/cars")
 public class CarsController extends HttpServlet {
 
   private CarsModel model = new CarsModel();
 
+  // DELETE
+  @Override
+  protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("Delete");
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("cars Delete");
+  }
+
+  // UPDATE
+  @Override
+  protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("Update");
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    out.println("cars Update" + request.getParameter("make"));
+  }
+
   // POST
+  @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String make = request.getParameter("make");
-    String color = request.getParameter("color");
-    String licensenumber = request.getParameter("licensenumber");
-    Car car = new Car(make, color, licensenumber);
-    model.add(car);
-    response.sendRedirect("cars");
+    String method = request.getParameter("_method");
+    System.out.println(method+" -------------");
+    switch (method) {
+      case "PUT":
+        doPut(request, response);
+        break;
+      case "DELETE":
+        doDelete(request, response);
+        break;
+      default:
+        String make = request.getParameter("make");
+        String color = request.getParameter("color");
+        String licensenumber = request.getParameter("licensenumber");
+        Car car = new Car(make, color, licensenumber);
+        model.add(car);
+        response.sendRedirect("cars");
+    }
   }
 
   // GET
+  @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     List<Car> cars = model.getAllCars();
     request.setAttribute("cars", cars);
     request.getRequestDispatcher("/pages/cars/cars.jsp").forward(request, response);
   }
-
 }
--- web/pages/cars/cars.jsp
+++ web/pages/cars/cars.jsp
@@ -24,7 +24,32 @@
 
 <div class="carsTable">
 
-    <%--<form class="form-inline" action="${pageContext.request.contextPath}/login" method="post">--%>
+    <%-- EDIT NEW CAR --%>
+    <h4>EDIT CAR</h4>
+    <form class="form-inline" action="${pageContext.request.contextPath}/cars" method="post">
+        <input type="hidden" name="_method" value="PUT"/>
+        <div class="form-group">
+            <p><label for="makePut">Make</label></p>
+            <input type="text" class="form-control" id="makePut" placeholder="Make" name="make">
+        </div>
+        <div class="form-group">
+            <p><label for="colorPut">Color</label></p>
+            <input type="text" class="form-control" id="colorPut" placeholder="Color" name="color">
+        </div>
+        <div class="form-group">
+            <p><label for="licensenumberPut">Licensenumber</label></p>
+            <input type="text" class="form-control" id="licensenumberPut" placeholder="Licensenumber" name="licensenumber">
+        </div>
+        <div>
+            <p></p>
+            <button type="submit" class="btn btn-success">Add</button>
+        </div>
+    </form>
+    <p></p>
+
+
+<%-- ADD NEW CAR --%>
+    <h4>ADD NEW CAR</h4>
     <form class="form-inline" action="${pageContext.request.contextPath}/cars" method="post">
         <div class="form-group">
             <p><label for="make">Make</label></p>
@@ -42,11 +67,11 @@
             <p></p>
             <button type="submit" class="btn btn-success">Add</button>
         </div>
-
     </form>
-
     <p></p>
 
+
+    <%-- TABLE CARS --%>
     <table class="table table-condensed table-bordered" id="carsTable">
 
         <thead>
