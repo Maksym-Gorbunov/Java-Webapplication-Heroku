32a9160ba4fa3bc1ca5ba6865ccb81f67ec0c05f
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-10-26 14:28:29 +0200

    CarsTest -add

--- src/test/java/cars/CarsTest.java
+++ src/test/java/cars/CarsTest.java
@@ -4,9 +4,12 @@ import model.CarsModel;
 import model.beans.Car;
 import model.db.CarsDBInterface;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class CarsTest {
 
@@ -17,4 +20,14 @@ public class CarsTest {
   public void init(){
     model = new CarsModel(fakeDBHelper);
   }
+
+  @Test
+  public void add(){
+    Car car = new Car("Tesla", "black", "TTT123");
+    assertEquals(false, model.getAllCars().contains(car));
+    model.add(car);
+    assertEquals(true, model.getAllCars().contains(car));
+  }
+
+
 }
